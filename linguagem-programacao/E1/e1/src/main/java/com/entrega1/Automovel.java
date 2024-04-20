a3d54e94a6a880d107b8964402ae8eeefbe71193
Elbert Jean <elbertjeandossantos@gmail.com>
2024-04-20 12:47:10 -0300

    atualizacao repositorio

---
+++ linguagem-programacao/E1/e1/src/main/java/com/entrega1/Automovel.java
@@ -0,0 +1,11 @@
+package com.entrega1;
+
+public class Automovel {
+    public String modelo;
+    public String ano;
+
+    public Automovel(String modelo, String ano) {
+        this.modelo = modelo;
+        this.ano = ano;
+    }
+}
